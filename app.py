4a32daec3529e5efbebd2c31408f45af8c5c6c7b
ketelinwliane <ketelinwliane@gmail.com>
2026-09-10 08:38:50 -0400

    Adiconando os arquivos de backend

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from config import Config
+from models import db
+from routes_inscricao import inscricao_bp
+from routes_menu import menu_bp
+from flask_mail import Mail
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    mail.init_app(app)
+
+    app.register_blueprint(inscricao_bp)
+    app.register_blueprint(menu_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5000)
---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Inscrito(db.Model):
+    __tablename__ = 'inscritos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Inscrito {self.nome}>'
